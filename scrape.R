77f9027d2aefd1116c754d13583b28e890357f3f
Nur Khamidah <nurkhamidah.stat@gmail.com>
2023-06-06 12:42:25 +0700

    [R] Finish code R

--- scrape.R
+++ scrape.R
@@ -44,4 +44,30 @@ con <- mongo(collection = collection,
              db = db,
              url = url)
 
+#------------------------------------------------------
+message("Store data to database")
+data <- list(
+  id = con$count()+1,
+  date = Sys.Date(),
+  time = Sys.time(),
+  aqi_value = data.frame(score = aqi_score,
+                         unit = aqi_unit,
+                         desc = aqi_desc),
+  pm25_value = data.frame(score = pm25_score,
+                          unit = pm25_unit,
+                          desc = pm25_desc),
+  recommendation = recommendation,
+  aqi_local_rank = aqi_rank_local,
+  aqi_indo_rank = aqi_rank_indo,
+  weather_info = data.frame(weather = info$X2[1],
+                            temperature = info$X1[2],
+                            humidity = info$X1[3],
+                            wind = info$X1[4],
+                            pressure = info$X1[5])
+)
+con$insert(data)
+
+#------------------------------------------------------
+message("Disconnect database")
+con$disconnect()
 
